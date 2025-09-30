18cc6aefb61721b42be2b9c2bb5bb952c4024b91
Your Name <you@example.com>
2025-09-30 22:20:11 +0200

    add derive macro EbiInputEnum

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,32 @@ extern crate quote;
 use proc_macro::TokenStream;
 use quote::quote;
 #[allow(unused_imports)]
-use syn::{parse_macro_input, DeriveInput, Ident};
+use syn::{DeriveInput, Ident, parse_macro_input};
+
+#[proc_macro_derive(EbiInputEnum)]
+pub fn derive2(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input);
+    let DeriveInput { ident, .. } = input;
+
+    let output = quote! {
+        impl FromEbiTraitObject for #ident {
+            fn from_trait_object(object: EbiInput) -> Result<Box<Self>> {
+                match object {
+                    EbiInput::String(value, _) => Ok(Box::new(value.parse()?)),
+                    _ => Err(anyhow!(
+                        "cannot read {} {} as an enum",
+                        object.get_type().get_article(),
+                        object.get_type()
+                    )),
+                }
+            }
+        }
+    };
+    output.into()
+}
 
 #[proc_macro_derive(ActivityKey)]
-pub fn derive(input: TokenStream) -> TokenStream { 
+pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input);
     let DeriveInput { ident, .. } = input;
 
@@ -16,7 +38,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             fn activity_key(&self) -> &ActivityKey {
                 &self.activity_key
             }
-        
+
             fn activity_key_mut(&mut self) -> &mut ActivityKey {
                 &mut self.activity_key
             }
