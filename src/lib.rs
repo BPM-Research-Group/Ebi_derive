3aad1b73b8adfd7be58f1c24fec081874fdc6052
Your Name <you@example.com>
2025-09-30 22:40:31 +0200

    add a meta-derive macro for EbiInputEnum

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,40 @@ use proc_macro::TokenStream;
 use quote::quote;
 #[allow(unused_imports)]
 use syn::{DeriveInput, Ident, parse_macro_input};
+use syn::{ItemStruct, parse_quote};
 
-#[proc_macro_derive(EbiInputEnum)]
-pub fn derive2(input: TokenStream) -> TokenStream {
+//credit to Yandros: https://users.rust-lang.org/t/derive-macro-aggregating-several-derive-macros/80222/11
+#[doc(hidden)]
+#[proc_macro_attribute]
+pub fn __annihilate(_: TokenStream, _: TokenStream) -> TokenStream {
+    <_>::default()
+}
+
+#[proc_macro_derive(
+    EbiInputEnum,
+    // attributes(sql_type), /* in case you wanted to allow extra `#[sql_typ>e]` annotations under your derive */
+)]
+pub fn aggregate_macro(item: TokenStream) -> TokenStream {
+    let mut item: ItemStruct = parse_macro_input!(item);
+    // ensure the generated item definition does not end up actually emitted.
+    item.attrs.push(parse_quote!(
+        #[::ebi_derive::__annihilate]
+    ));
+    // "re"-emit the item with the desired derives
+    quote!(
+        #[::core::prelude::v1::derive(
+            ::ebi_derive::EbiInputEnumFromEbiTraitObject,
+            ::strum_macros::VariantNames,
+            ::strum_macros::EnumString,
+        )]
+        #[strum(serialize_all = "lowercase")]
+        #item
+    )
+    .into()
+}
+
+#[proc_macro_derive(EbiInputEnumFromEbiTraitObject)]
+pub fn derive_from_ebi_trait_object(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input);
     let DeriveInput { ident, .. } = input;
 
